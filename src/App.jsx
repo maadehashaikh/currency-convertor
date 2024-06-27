5489773a0ecc550d9d96f028a8849dc0fb6f0bc8
Maadeha shaikh <maadeham@gmail.com>
2024-06-27 18:51:38 +0500

    Updated !!

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
+    
     <>
       <h1 className='text-3xl bg-green-400 text-white hover:bg-transparent hover:text-black'>Currency Convertor ! </h1>
     </>
