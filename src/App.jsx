080c19dc2a40ce20468cc5ccb5a02cd95df390ad
Maadeha shaikh <maadeham@gmail.com>
2024-06-28 23:41:01 +0500

    Updated !!

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,10 @@ import './App.css'
 
 function App() {
   return (
-    
+
     <>
       <h1 className='text-3xl bg-green-400 text-white hover:bg-transparent hover:text-black'>Currency Convertor ! </h1>
     </>
   )
 }
-
 export default App
