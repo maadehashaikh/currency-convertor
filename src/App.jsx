39c2feb08da1e8dbeaa72c379b5990117a8b95f6
Maadeha shaikh <maadeham@gmail.com>
2024-07-01 23:48:10 +0500

    Updated !!

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
     <>
       <h1 className='text-3xl bg-green-400 text-white hover:bg-transparent hover:text-black 
-      py-4 left-4 text-lime-300'>Currency Convertor ! </h1>
+      py-4 left-4 text-lime-300 mx-1'>Currency Convertor ! </h1>
     </> 
   )
 }
